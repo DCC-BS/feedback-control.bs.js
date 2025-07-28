ea6a443ab30a6d8a9998c3194141651013cd6c30
GitHub Action <action@github.com>
2025-07-28 11:18:29 +0000

    Release v0.1.4

---
+++ dist/runtime/components/feedback-control.vue.d.ts
@@ -0,0 +1,7 @@
+interface InputProps {
+    defaultMail?: string;
+}
+declare const _default: import("vue").DefineComponent<InputProps, void, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<InputProps> & Readonly<{}>, {
+    defaultMail: string;
+}, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
+export default _default;
--- playground/app.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-    <FeedbackControl default-mail="deine@email.de"/>
-</template>
